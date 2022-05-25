fb6c63c3c2828c14dbdbbbbc24539bec1440226e
Bharadwaj, Varun (ADV D AA PAMC NM DH1 ONG) <varun.bharadwaj@siemens.com>
2022-05-25 17:53:42 +0530

    Implementation of Color Coding Reference Manual through Utility Class

--- colorcoder/Main.java
+++ colorcoder/Main.java
@@ -1,12 +1,29 @@
 package colorcoder;
 
+import java.util.Map;
+
 public class Main {
 
 
     public static void main(String[] args) {
+
+        Main main = new Main();
         ColorPairHelper.testNumberToPair(4, MajorColor.WHITE, MinorColor.BROWN);
         ColorPairHelper.testNumberToPair(5, MajorColor.WHITE, MinorColor.SLATE);
         ColorPairHelper.testPairToNumber(MajorColor.BLACK, MinorColor.ORANGE, 12);
         ColorPairHelper.testPairToNumber(MajorColor.VIOLET, MinorColor.SLATE, 25);
+        Map<Integer,ColorPair> colorCodingPairMap = ColorPairUtil.getColorCodingPairMap();
+        main.printColorCodingReferenceManual(colorCodingPairMap);
+
+    }
+
+    private void printColorCodingReferenceManual(Map<Integer,ColorPair> colorCodingPairMap) {
+        System.out.println("**********************************");
+        System.out.println("Pair No.|| MajorColor || MinorColor");
+        for(Map.Entry<Integer,ColorPair> colorPairEntry : colorCodingPairMap.entrySet()) {
+            System.out.println(colorPairEntry.getKey()+" ---------"
+                    + colorPairEntry.getValue().getMajor()+" ----------"
+                    + colorPairEntry.getValue().getMinor());
+        }
     }
 }

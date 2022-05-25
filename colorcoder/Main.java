253ef528155b1e7eee39b47a0792ce7e3cbac4fc
Bharadwaj, Varun (ADV D AA PAMC NM DH1 ONG) <varun.bharadwaj@siemens.com>
2022-05-25 17:14:28 +0530

    Duplicate Conversion Logic refactored to single function to cater to major and minor color combinations

--- colorcoder/Main.java
+++ colorcoder/Main.java
@@ -6,7 +6,6 @@ public class Main {
     public static void main(String[] args) {
         ColorPairHelper.testNumberToPair(4, MajorColor.WHITE, MinorColor.BROWN);
         ColorPairHelper.testNumberToPair(5, MajorColor.WHITE, MinorColor.SLATE);
-
         ColorPairHelper.testPairToNumber(MajorColor.BLACK, MinorColor.ORANGE, 12);
         ColorPairHelper.testPairToNumber(MajorColor.VIOLET, MinorColor.SLATE, 25);
     }
